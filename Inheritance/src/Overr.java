5a86f4467bc9048e994a1284c4fca708e6ef8454
USER <USER@DESKTOP-Q1V3M8B>
2026-02-05 22:53:54 +0530

    final commit

---
+++ Inheritance/src/Overr.java
@@ -0,0 +1,29 @@
+
+
+/**
+ * 
+ * @author {Kireet Joshi}
+ *
+ */
+class P
+{
+	Object m()
+	{
+	System.out.println("hello");
+	return null;
+}
+}
+class Q extends P
+{
+	String m()
+	{
+	System.out.println("hi");
+	return null;
+}
+}
+public class Overr {
+public static void main(String[] args) {
+	P op=new Q();
+	op.m();
+}
+}

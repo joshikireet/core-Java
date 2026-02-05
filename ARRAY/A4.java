d7ebe9144b8e523f9e65edb19510e4a7b326ca0f
USER <USER@DESKTOP-Q1V3M8B>
2026-02-05 21:41:51 +0530

    commit array...

---
+++ ARRAY/A4.java
@@ -0,0 +1,9 @@
+
+public class A4 {
+	public static void main(String[] args) {
+		int[][] x=new int[2][];
+		System.out.println(x);
+		System.out.println(x[0]);
+		System.out.println(x[0][0]);
+	}
+}

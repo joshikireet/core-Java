c44ebbbf5a86305e831985807fccead1616ff6f8
USER <USER@DESKTOP-Q1V3M8B>
2026-02-10 20:24:12 +0530

    private static interface commit

---
+++ Interface/src/Dummy1.java
@@ -0,0 +1,17 @@
+interface I7
+{
+	
+	static void foo1()
+	{
+		foo();
+	}
+	private static void foo()
+	{
+	System.out.println("private static method");
+	}
+}
+public class Dummy1 implements I7 {
+public static void main(String[] args) {
+	I7.foo1();// invoked only by interface_name.method_name
+}
+}

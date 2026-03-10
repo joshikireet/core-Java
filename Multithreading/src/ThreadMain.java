98251c7baa8cd2df46ca55de6c97bf7958281684
USER <USER@DESKTOP-Q1V3M8B>
2026-03-10 20:48:38 +0530

    commit

---
+++ Multithreading/src/ThreadMain.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ * @author {Kireet Joshi}
+ * THREAD CREATION USING THREAD CLASS
+ */
+class MyThread0 extends Thread
+{
+	public void run() 
+	{
+		for(int i=0;i<10;i++)
+			System.out.println("im a child thread..");
+}
+}
+public class ThreadMain 
+{
+	public static void main(String[] args)throws InterruptedException {
+MyThread0 mt = new MyThread0(); // new born thread
+mt.start(); // thread starts executing
+for(int i=0;i<10;i++)
+	System.out.println("Main thread!!");
+	}
+}
+

de84d88452b7a7dadff3b677ddcb64fc59b70af4
USER <USER@DESKTOP-Q1V3M8B>
2026-03-10 20:51:04 +0530

    commit

--- Multithreading/src/Main.java
+++ Multithreading/src/Main.java
@@ -1,4 +1,10 @@
+/**
+ * 
+ * @author {Kireet Joshi}
+ *
+ */
 //Thread creation using Runnable interface
+
 class MyRunnable implements Runnable
 {
 	public void run() 
@@ -15,7 +21,8 @@ public class Main
 	public static void main(String[] args) {	
 		MyRunnable r= new MyRunnable();
 		Thread t = new Thread(r);
-		r.run();// no new  thread is created  
+	     t.start();
+		//	r.run();// no new  thread is created  
 		for(int i=0;i<10;i++)
 			System.out.println("Main thread!!");
 	}
